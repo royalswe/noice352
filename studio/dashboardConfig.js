be22cba84fda8fd788b6568205758c1e0b2e4ac0
Sanity.io <hello@sanity.io>
2022-01-09 22:18:22 +0100

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,52 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'noice-352-studio',
+                  apiId: 'f60cce7c-d6ba-464c-abfc-4e07d95a8cf2'
+                },
+                {
+                  buildHookId: '',
+                  title: 'Blog Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/royalswe/noice352',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: '', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
